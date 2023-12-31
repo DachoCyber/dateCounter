3b91b3cb08f22a3efc83f472981e24f01be2e99a
DachoCyber <dalibormax100@gmail.com>
2023-12-31 21:31:56 +0100

    secondBranch

---
+++ test.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "dates.h"
+
+tests() {
+	Date date1[6] = {{3, 7, 2002}, {1, 12, 2002}, {1, 1, 1}, {1, 3, 2005}},
+	date2[6] = {{4, 7, 2002}, {}};
+	int correctAnswer[6];
+	
+}
